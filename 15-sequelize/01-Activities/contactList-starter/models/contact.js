af6643721f14157b14d6844e324d62e7d6acd641
Scott Bickell <bickellworks@gmail.com>
2019-02-01 19:44:40 -0700

    Add contact list app from last week's Sequelize class

---
+++ 15-sequelize/01-Activities/contactList-starter/models/contact.js
@@ -0,0 +1,42 @@
+module.exports = function (sequelize, DataTypes) {
+    var Contact = sequelize.define("Contact", {
+        // firstName (VARCHAR, NOT NULL, between 1-30 characters)
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 50]
+            }
+        },
+        // lastName (VARCHAR, NOT NULL, between 1-30 characters)
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 50]
+            }
+        },
+        // contactType (VARCHAR, Default value "Personal")       
+        contactType: {
+            type: DataTypes.STRING,
+            defaultValue: "Personal"
+        },
+        // phoneNumber (VARCHAR, NULL, length 10 characters, numbers only)
+        phoneNumber: {
+            type: DataTypes.STRING,
+            validate: {
+                len: [10, 10],
+                is: /^[0-9]+$/
+            }
+        },
+        // emailAddress (VARCHAR, NULL, must be valid email format)
+        emailAddress: {
+            type: DataTypes.STRING,
+            validate: {
+                isEmail: true
+            }
+        }
+    });
+    // be sure to return the model!
+    return Contact;
+};
