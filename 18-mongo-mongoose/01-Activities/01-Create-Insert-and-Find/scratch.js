bfd9150e654eb89b66a1fb043026bf802caaea74
Scott Bickell <bickellworks@gmail.com>
2019-02-25 20:59:55 -0700

    Add latest

---
+++ 18-mongo-mongoose/01-Activities/01-Create-Insert-and-Find/scratch.js
@@ -0,0 +1,3 @@
+db.places.insert({"continent": "Africa", "country":"Morocco", "majorcities": ["Casablanca", "Fez", "Marrakech"]})
+db.places.insert({"continent": "Australia", "country":"Australia", "majorcities": ["Melborne", "Darwin", "Sydney"]})
+db.places.insert({"continent": "North America", "country":"United States", "majorcities": ["New York", "Los Angeles", "Chicago", "Denver"]})
--- 18-mongo-mongoose/01-Activities/05-Scraping/server.js
+++ 18-mongo-mongoose/01-Activities/05-Scraping/server.js
@@ -14,6 +14,7 @@ axios.get("https://old.reddit.com/r/webdev/").then(function(response) {
 
   // Load the HTML into cheerio and save it to a variable
   // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+  // Doesn't have to be "$". Can be any valid variable name
   var $ = cheerio.load(response.data);
 
   // An empty array to save the data that we'll scrape
