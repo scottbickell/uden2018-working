7eb1a2a90ddf0d58c9c1a967cb8861d15db21c02
Scott Bickell <bickellworks@gmail.com>
2019-03-18 18:30:11 -0600

    Add pupster files

--- 20-react/01-Activities/06-Stu_PupsterApp/pupster/src/App.js
+++ 20-react/01-Activities/06-Stu_PupsterApp/pupster/src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavTabs from "./components/NavTabs";
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import Discover from "./components/pages/Discover";
-import Search from "./components/pages/Search";
+import Discover from "./pages/Discover";
+import About from "./pages/About";
+import Search from "./pages/Search";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
     <Router>
       <div>
-        <NavTabs />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/discover" component={Discover} />
-        <Route path="/Search" component={Search} />
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/discover" component={Discover} />
+          <Route exact path="/search" component={Search} />
+        </Wrapper>
+        <Footer />
       </div>
     </Router>
   );
